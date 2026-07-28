255f00b3adaf946870d3db00092bd98806183718
liushuai <770722922@qq.com>
2026-07-28 17:04:40 +0800

    feat: enable WiFi OTA uploads by default

--- src/config.h
+++ src/config.h
@@ -2,6 +2,8 @@
 #define TMS 1000              //一千毫秒
 #define WM_EN 1               // WEB配网使能标志位----WEB配网打开后会默认关闭smartconfig功能
 #define DHT_EN 0              //设定DHT11温湿度传感器使能标志
+#define OTA_EN 1              //启用 WiFi OTA 上传；首次仍需通过串口刷入
+#define OTA_HOSTNAME "SmallDesktopDisplay"
 #define SD_FONT_YELLOW 0xD404 // 黄色字体颜色
 #define SD_FONT_WHITE 0xFFFF  // 黄色字体颜色
 
